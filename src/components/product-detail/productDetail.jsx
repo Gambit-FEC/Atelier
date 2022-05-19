a77e46471ad51d401a97cb825d67f0b640dc3885
Esther Kuang <eghkuang@gmail.com>
2022-05-19 12:13:04 -0700

    edits to productDetail

--- src/components/product-detail/productDetail.jsx
+++ src/components/product-detail/productDetail.jsx
@@ -1,51 +1,43 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React from 'react';
+import { useId } from '../../context/GlobalStore';
 // import axios from 'axios';
 // import styled from 'styled-components';
 
-export const UserContext = React.createContext();
-
-
-
-//styled-components ex
-// Create a Title component that'll render an <h1> tag with some styles
-const ProductDetailContainer = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-// Create a Wrapper component that'll render a <section> tag with some styles
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
-
+// //styled-components ex
+// // Create a Title component that'll render an <h1> tag with some styles
+// const ProductDetailContainer = styled.h1`
+//   font-size: 1.5em;
+//   text-align: center;
+//   color: palevioletred;
+// `;
+// // Create a Wrapper component that'll render a <section> tag with some styles
+// const Wrapper = styled.section`
+//   padding: 4em;
+//   background: papayawhip;
+// `;
 
 export default function ProductDetail() {
-  //test------
-  const [product, setProduct] = useState([]);
-
-  const inputState = useState({ title: '', amount: ''});
-
-
-
-  function User() {
-    const value = React.useContext(UserContext);
-
-    return <h1>{value}</h1>;
-  }
-
+  // test------
+  const [productId, setProductId] = useId();
 
   return (
-    <Wrapper>
-      <ProductDetailContainer>
+    <>
+      <h1>{productId}</h1>
+      <h2>productDetail</h2>
+      <div>wow</div>
+      <p>hello</p>
+    </>
 
-        <div>
-          <h2>productDetail</h2>
-          <div>wow</div>
-          <p>hello</p>
-        </div>
+  // <Wrapper>
+  //   <ProductDetailContainer>
 
-      </ProductDetailContainer>
-    </Wrapper>
-  )
-};
+  // <div>
+  // <h2>productDetail</h2>
+  // <div>wow</div>
+  // <p>hello</p>
+  // </div>
+
+  //   </ProductDetailContainer>
+  // </Wrapper>
+  );
+}
