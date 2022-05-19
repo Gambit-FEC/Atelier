e7462010c34725f9377784112b654aab674d5069
gilcohen67 <gil.cohen67@gmail.com>
2022-05-19 10:53:49 -0700

    axios get context

--- src/context/GlobalStore.jsx
+++ src/context/GlobalStore.jsx
@@ -1,5 +1,19 @@
-import { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import axios from 'axios';
+import { useCallback } from 'react/cjs/react.production.min';
 
-const GlobalContext = createContext();
+const IdContext = createContext();
+const fetchByIdContext = createContext();
 
-export default GlobalContext;
+export default function IdContextProvider({ children }) {
+  const [id, setId] = useState(40348);
+  const fetchById = useCallback((route, config) => axios.get(route, config));
+
+  return (
+    <IdContext.Provider value={id}>
+      <fetchByIdContext.Provider value={fetchById}>
+        {children}
+      </fetchByIdContext.Provider>
+    </IdContext.Provider>
+  );
+}
