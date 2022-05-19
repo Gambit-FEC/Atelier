a5ce8f02ad7617b6c10e0a9a7da2660bfd8bc155
Andy Luu <luuandyf2nd@gmail.com>
2022-05-19 12:29:51 -0700

    Renamed main related items file and implemented basic skeleton structure for related items module

--- src/components/related-items/RelatedItems.jsx
+++ src/components/related-items/RelatedItems.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useId } from '../../context/GlobalStore';
+import RelatedList from './related/relatedList';
+import OutfitList from  './outfit/outfitList';
 
 export default function RelatedItems() {
   const [productId, setProductId] = useId();
   return (
     <>
-      <h1>{productId}</h1>
+      <h1>RELATED ITEMS MODULE ITEM ID: {productId}</h1>
+      <RelatedList/>
+      <OutfitList/>
     </>
   );
 }
