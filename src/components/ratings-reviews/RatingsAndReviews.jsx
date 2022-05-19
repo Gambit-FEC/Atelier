f3ed0d42d3ca5eb44b928b76d38de6a83efbfe1d
gilcohen67 <gil.cohen67@gmail.com>
2022-05-19 14:59:45 -0700

    class named

--- src/components/ratings-reviews/RatingsAndReviews.jsx
+++ src/components/ratings-reviews/RatingsAndReviews.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 // import { RatingsAndReviewsProvider } from './context/RatingsAndReviewsContext';
 import Reviews from './reviews/Reviews';
-import { useId, updateId } from '../../context/GlobalStore';
 
 export default function RatingsAndReviews() {
-  const id = useId();
-  const setId = updateId();
   return (
-    <>
-      <h1>{id}</h1>
-      <button type="button" onClick={() => setId(id + 1)}>Increment</button>
-    </>
-    // <RatingsAndReviewsProvider>
-    //   {/* <Ratings /> */}
-    //   {/* <Reviews /> */}
-    // </RatingsAndReviewsProvider>
+    <div className="ratings-and-reviews">
+      {/* <Ratings /> */}
+      <Reviews />
+    </div>
   );
 }
