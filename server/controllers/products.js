597796bfc627caa19f089d78fa72a8b08f517126
Esther Kuang <eghkuang@gmail.com>
2022-05-20 15:14:24 -0700

    got api connection to work with controller side

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
-const { API_KEY } = require('../../config');
 
 exports.getProductInfo = (req, res) => {
   // eslint-disable-next-line no-console
   console.log('do i work?');
-  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products?product_id=${req.query.id}`, { headers: { Authorization: API_KEY } })
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${req.params.productId}`, { headers: { Authorization: req.headers.Authorization } })
     .then((data) => { res.status(200).send(data.data); })
     .catch((err) => { console.log('err', err); res.status(500).send(err); });
   // res.sendStatus(200);
