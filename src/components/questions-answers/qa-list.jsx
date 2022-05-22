805b141683c1bae424373c5f43648ea01f80b681
danika-gray <danika.partridge@gmail.com>
2022-05-21 19:26:13 -0700

    save work, currently able to get data in qa-list and send each question object to qa-tile and can then display/format data in qa-tile and print it to the page

--- src/components/questions-answers/qa-list.jsx
+++ src/components/questions-answers/qa-list.jsx
@@ -1,22 +1,38 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useGlobalContext } from '../../context/GlobalStore';
 import { API_KEY, API_URL } from './config';
+import TileQA from './qa-tile';
 
 function ListQA() {
   // gets data for questions from api
   // gets data for answers from api
   // organizes data into QA tiles and passes data into a QA-Tile component for each tile
   const { productId } = useGlobalContext();
-  console.log(productId);
+  const [questions, setQuestions] = useState([]);
 
-  axios.get(`${API_URL}qa/questions`, { params: { product_id: productId }, headers: { Authorization: API_KEY } })
-    .then((res) => {
-      console.log(res.data, 'res.data');
-    })
-    .catch((err) => {
-      alert(err);
-    });
+  const getData = () => {
+    axios.get(`${API_URL}qa/questions`, { params: { product_id: productId }, headers: { Authorization: API_KEY } })
+      .then((res) => {
+        console.log(res.data, 'res.data');
+        console.log(res.data.results, 'res.data.results');
+        // setQuestions(res.data.results); // set new value of questions
+        setQuestions(res.data.results);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  useEffect(() => { // when component mounts get the data
+    getData();
+  }, []);
+
+  console.log(questions, 'questions');
+
+  return (
+    <div>{questions.map((question) => <TileQA question={question} />)}</div>
+  );
 }
 
 export default ListQA;
