4b51585f8e5fd5a931a94e59afab6bc212424bec
gilcohen67 <gil.cohen67@gmail.com>
2022-05-23 15:33:56 -0700

    date and photos showing but readmore doesn't reset

--- src/components/ratings-reviews/reviews/reviews-list/ReviewTile.jsx
+++ src/components/ratings-reviews/reviews/reviews-list/ReviewTile.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
+import { format, parseISO } from 'date-fns';
 
 export default function ReviewTile({ review }) {
-  const [readMore, setReadMore] = useState(false);
+  const [readMore, setReadMore] = useState(() => false);
   function handleReadMoreClick() {
     setReadMore(!readMore);
   }
-
+  console.log(review);
+  function showPhotos() {
+    return review.photos.map((item) => <img key={item.id} src={item.url} alt={`review-${item.id}`} />);
+  }
   return (
     <div className="reviews-tile">
-      <div>{review.rating}</div>
-      <div>{review.date}</div>
-      {!readMore && <div>{review.summary}</div>}
-      {readMore && <div>{review.body}</div>}
+      <div>
+        {review.rating}
+        ⭐
+      </div>
+      <div>{format(parseISO(review.date), 'MMM dd, yyyy')}</div>
+      {!readMore && <div style={{ fontWeight: 'bold' }}>{review.summary}</div>}
+      {readMore && <p>{review.body}</p>}
+      {readMore && (
+        <>
+          {showPhotos()}
+        </>
+      )}
       {!readMore && <button type="button" onClick={handleReadMoreClick}>Read more</button>}
     </div>
   );
