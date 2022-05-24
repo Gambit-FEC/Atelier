87e78c9ba18b27c4fa3badf85be80b07657da811
gilcohen67 <gil.cohen67@gmail.com>
2022-05-24 14:32:53 -0700

    format

--- src/components/ratings-reviews/reviews/reviews-list/ReviewsList.jsx
+++ src/components/ratings-reviews/reviews/reviews-list/ReviewsList.jsx
@@ -4,7 +4,6 @@ import { RatingsAndReviewsContext } from '../../RatingsAndReviews';
 
 export default function ReviewsList() {
   const { reviews, reviewsFilter } = useContext(RatingsAndReviewsContext);
-  console.log('filter:', reviewsFilter)
   return (
     <div className="reviews-list">
       {reviews.map((review, idx) => {
