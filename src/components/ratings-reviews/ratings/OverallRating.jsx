c1ea700a32ca617207cbaf8fb1d3672e00a6b042
gilcohen67 <gil.cohen67@gmail.com>
2022-05-24 00:01:47 -0700

    global rating stars styled-component

--- src/components/ratings-reviews/ratings/OverallRating.jsx
+++ src/components/ratings-reviews/ratings/OverallRating.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
 import { useGlobalContext } from '../../../context/GlobalStore';
 import { RatingsAndReviewsContext } from '../RatingsAndReviews';
+import StyledRatingStars from '../../../styled-lib';
 
 export default function OverallRating() {
   const { setReviewsMeta } = useContext(RatingsAndReviewsContext);
@@ -10,7 +10,7 @@ export default function OverallRating() {
   useEffect(() => {
     axios.get(`/reviews/meta/${productId}`)
       .then(({ data }) => {
-        console.log('fetched average', data);
+        console.log('fetched average');
         setReviewsMeta(data);
         setAvgRating(data.averageRating);
       })
@@ -18,22 +18,11 @@ export default function OverallRating() {
         console.log('Error fetching average ratings:', err);
       });
   }, []);
-  const StyledRatingStars = styled.div`
-    display: inline-block;
-    font-size: xxx-large;
-    background: linear-gradient(to right, black ${avgRatingToPercent()[0]}, lightgrey ${avgRatingToPercent()[1]});
-    background-clip: text;
-    -webkit-background-clip: text;
-    color: transparent;
-  `;
-  function avgRatingToPercent() {
-    return [`${avgRating * 20}%`, `${100 - avgRating * 20}%`];
-  }
+
   return (
     <div className="ratings-overall">
-      <div style={{ fontSize: '48px' }}>{avgRating}</div>
-      {/* <div className="rating-stars" style={{fontSize: 'xxx-large', background: `linear-gradient(to right, black ${avgRatingToPercent()[0]}, white ${avgRatingToPercent()[1]})`}}>☆☆☆☆☆</div> */}
-      <StyledRatingStars>☆☆☆☆☆</StyledRatingStars>
+      <span style={{ fontSize: '48px' }}>{avgRating}</span>
+      <StyledRatingStars rating={avgRating}>☆☆☆☆☆</StyledRatingStars>
     </div>
   );
 }
---
+++ src/styled-lib/index.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+function avgRatingToPercent(rating) {
+  return [`${rating * 20}%`, `${100 - rating * 20}%`];
+}
+
+const StyledRatingStars = styled.div`
+  display: inline-block;
+  font-size: xxx-large;
+  background: linear-gradient(to right, black ${({ rating }) => avgRatingToPercent(rating)[0]}, lightgrey ${({ rating }) => avgRatingToPercent(rating)[1]});
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+`;
+
+export default StyledRatingStars;
