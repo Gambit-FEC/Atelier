b14459d2adc74f2eed19bc5ae3b63d7add89b0f2
gilcohen67 <gil.cohen67@gmail.com>
2022-05-25 16:57:25 -0700

    added star review and recommended check

--- src/components/ratings-reviews/write-review/WriteReview.jsx
+++ src/components/ratings-reviews/write-review/WriteReview.jsx
@@ -4,8 +4,8 @@ import { useRAndRContext } from '../../../context/RAndRContext';
 export default function WriteReview() {
   const { setShowWriteReview } = useRAndRContext();
   const [formData, setFormData] = useState({
-    overallRating: '',
-    recommend: '',
+    overallRating: 0,
+    recommend: false,
     characteristics: {},
     summary: '', // optional
     body: '',
@@ -14,27 +14,53 @@ export default function WriteReview() {
     email: '',
   });
   function handleExitView() {
+    document.body.style.overflow = 'auto';
     setShowWriteReview(false);
   }
-  function handleSubmitReview(e) {
-    for (let i = 0; i < e.target.length - 1; i++) {
-      console.log(e.target[i].value);
-    }
+  function handleSubmitReview() {
+    console.log(formData);
   }
   function updateFormData(e) {
+    console.log(e.target.value);
     const newData = { ...formData };
     newData[e.target.id] = e.target.value;
     setFormData(newData);
   }
+  function handleStarClick(e) {
+    if (formData.overallRating === e.target.id) {
+      setFormData({ ...formData, overallRating: 0 });
+    } else {
+      setFormData({ ...formData, overallRating: e.target.id });
+    }
+  }
+  function handleCheckBox() {
+    setFormData({ ...formData, recommend: !formData.recommend });
+  }
+  function starRender() {
+    const spans = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < formData.overallRating) {
+        spans.push(<span key={i} id={i + 1} style={{ cursor: 'pointer' }} onClick={handleStarClick}>★</span>);
+      } else {
+        spans.push(<span key={i} id={i + 1} style={{ cursor: 'pointer' }} onClick={handleStarClick}>☆</span>);
+      }
+    }
+    return (
+      <div>
+        {spans}
+      </div>
+    );
+  }
+
   return (
     <div className="write-review modal-bg">
       <form className="modal-form" onSubmit={handleSubmitReview}>
         <div className="modal-exit-button" onClick={handleExitView}>[X]</div>
         <h3 style={{ textAlign: 'center' }}>Write A Review</h3>
         <label>Overall Rating:</label>
-        <textarea id="overallRating" onChange={updateFormData} />
+        {starRender()}
         <label>Do you recommend this product?</label>
-        <textarea id="recommend" />
+        <input id="recommend" type="checkbox" defaultChecked={formData.recommend} onChange={handleCheckBox} />
         <label>Characteristics:</label>
         <textarea id="characteristics" />
         <label>Summary:</label>
