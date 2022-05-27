b68cf969508717101f532f6450c889dd8ae4bf58
Andy Luu <luuandyf2nd@gmail.com>
2022-05-26 18:34:17 -0700

    Fixed the carousel issue, made some adjustments to Empty outfit card

--- src/components/related-items/outfit/outfitCard.jsx
+++ src/components/related-items/outfit/outfitCard.jsx
@@ -6,13 +6,11 @@ import { StyledRatingStars } from '../../../styled-lib';
 
 export function EmptyCard({ addCard }) {
   return (
-    <CardWrapper>
-      <StyledCard onClick={() => addCard()}>
-        <InfoWrapper>
-          EMPTY CARD
-        </InfoWrapper>
-      </StyledCard>
-    </CardWrapper>
+    <StyledCard onClick={() => addCard()}>
+      <InfoWrapper>
+        EMPTY CARD
+      </InfoWrapper>
+    </StyledCard>
   );
 }
 
@@ -35,85 +33,92 @@ export function OutfitCard({ data, removeCard, addCard }) {
   }, [data]);
 
   return (
-    <Container>
+    <Container className="Container">
+      <EmptyCard addCard={() => { addCard(); }} />
       {
         current !== 0
           ? <FcPrevious className="left-arrow" onClick={prevSlide} />
           : null
       }
       {
-        current !== 0
+        current === 4
           ? <FcNext className="right-arrow" onClick={nextSlide} />
           : null
       }
-      <CardWrapper>
-        <EmptyCard addCard={() => { addCard(); }} />
-        {
-          display.map((info, index) => (
-            <StyledCard key={index}>
-              <GiCancel onClick={() => { removeCard(info.product.id); }} />
-              {info.product.name}
-              <ImageWrapper>
-                <StyleImg src={
+      {
+        display.map((info, index) => (
+          <StyledCard key={index}>
+            <GiCancel onClick={() => { removeCard(info.product.id); }} />
+            <StyleImg src={
               info.style.thumbnail_url === null ? placeholder : info.style.thumbnail_url
-              } />
-              </ImageWrapper>
-              <InfoWrapper>
+            }
+            />
+            <InfoWrapper value={info.product.id}>
+              <CategoryWrapper>
                 {info.product.category}
+              </CategoryWrapper>
+              <NameWrapper>
                 {info.product.name}
+              </NameWrapper>
+              <PriceWrapper>
                 $
                 {info.product.price}
-                <StyledRatingStars rating={info.rating.averageRating}>
-                  ★★★★★
-                </StyledRatingStars>
-              </InfoWrapper>
-            </StyledCard>
-          ))
-        }
-      </CardWrapper>
+              </PriceWrapper>
+              <StyledRatingStars rating={info.rating.averageRating}>
+                ★★★★★
+              </StyledRatingStars>
+            </InfoWrapper>
+          </StyledCard>
+        ))
+      }
     </Container>
   );
 }
-
 const Container = styled.div`
-  position: relative;
-  justify-content: space-evenly;
-  align-items: center;
-`;
-
-const CardWrapper = styled.div`
-  display:flex;
-  object-fit:cover;
+display: flex;
+align-items: center;
+justify-content: flex-start;
 `;
 
 const StyledCard = styled.div`
-    display: flex;
-    border-radius: 10px;
-    padding: 15px;
-    border-width: 5px;
-    border-style: solid;
-    max-width: 300px;
-    width: 20%;
-    height: 420px;
-    margin: 10px;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    align-items: center;
-    justify-content: space-between;
-    &:hover {
-      box-shadow: 0 0 10px rgba(90, 90, 90, 0.8)
-    }
+border-radius: 10px;
+padding: 15px;
+border-width: 5px;
+border-style: solid;
+max-width: 300px;
+width: 20%;
+height: 420px;
+margin: 10px;
+align-items: center;
+justify-content: space-between;
+&:hover {
+  box-shadow: 0 0 10px rgba(90, 90, 90, 0.8)
+}
 `;
 
 const StyleImg = styled.img`
-  width: 100%;
-  height: 250px;
-  background-style: contain;
+width: 100%;
+height: 250px;
+object-fit: contain;
 `;
 
 const InfoWrapper = styled.div`
   background-color: white;
 `;
-const ImageWrapper = styled.div`
-  background-color: grey;
+
+const CategoryWrapper = styled.p`
+font-weight: normal;
+text-transform: uppercase;
+font-size: 16px;
+align-items: center;
+`;
+const NameWrapper = styled.p`
+font-weight: bold;
+font-size: 18px;
+align-items: center;
+`;
+const PriceWrapper = styled.p`
+font-weight: normal;
+font-size: 16px;
+align-items: center;
 `;
