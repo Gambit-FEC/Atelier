7780886878e934f12dddad2ed7fa014100391bdf
gilcohen67 <gil.cohen67@gmail.com>
2022-06-01 09:21:30 -0700

    navbar file added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GlobalContextProvider } from '../context/GlobalStore';
 import { RAndRContextProvider } from '../context/RAndRContext';
+import NavBar from './NavBar';
 import ProductDetail from './product-detail/productDetail';
 import RelatedItems from './related-items/RelatedItems';
 import RatingsAndReviews from './ratings-reviews/RatingsAndReviews';
@@ -8,18 +9,11 @@ import RatingsAndReviews from './ratings-reviews/RatingsAndReviews';
 function App() {
   return (
     <>
-      <div className="page-top-bar">
-        <div className="page-title">Gambit Outlet</div>
-        <div className="nav-bar">
-          <a className="underline-button nav-bar-item" href="#product-detail">Product Overview</a>
-          <a className="underline-button nav-bar-item" href="#related-items">Related Products & Outfit</a>
-          <a className="underline-button nav-bar-item" href="#ratings-and-reviews">Ratings and Reviews</a>
-        </div>
-      </div>
+      <NavBar />
       <GlobalContextProvider>
-        <ProductDetail />
+        {/* <ProductDetail /> */}
         {/* <QuestionsAndAnswers /> */}
-        <RelatedItems />
+        {/* <RelatedItems /> */}
         <RAndRContextProvider>
           <RatingsAndReviews />
         </RAndRContextProvider>
