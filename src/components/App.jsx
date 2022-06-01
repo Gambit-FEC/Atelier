c5dd986b6c8bb4cf80b0ccb568a02eb32d793c2d
gilcohen67 <gil.cohen67@gmail.com>
2022-05-31 20:56:15 -0700

    fixed id names

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,8 +11,8 @@ function App() {
       <div className="page-top-bar">
         <div className="page-title">Gambit Outlet</div>
         <div className="nav-bar">
-          <a className="underline-button nav-bar-item" href="#productDetail">Product Overview</a>
-          <a className="underline-button nav-bar-item" href="#related-products-module">Related Products & Outfit</a>
+          <a className="underline-button nav-bar-item" href="#product-detail">Product Overview</a>
+          <a className="underline-button nav-bar-item" href="#related-items">Related Products & Outfit</a>
           <a className="underline-button nav-bar-item" href="#ratings-and-reviews">Ratings and Reviews</a>
         </div>
       </div>
