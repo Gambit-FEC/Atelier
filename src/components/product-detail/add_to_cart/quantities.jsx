c7cafcb25f4f147c116b8c518e62de515d827312
Esther Kuang <eghkuang@gmail.com>
2022-06-01 11:37:51 -0700

    removed unused onChange function in quantities

--- src/components/product-detail/add_to_cart/quantities.jsx
+++ src/components/product-detail/add_to_cart/quantities.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function SelectQuantity({ quantities, onChange }) {
+export default function SelectQuantity({ quantities }) {
   return (
     (!quantities.length) ? <Quantity disabled>---</Quantity> : (
-      <Quantity name="Select Quantity" onChange={(e) => onChange(e)}>
+      <Quantity name="Select Quantity">
         <option defaultValue>1</option>
         {quantities.map((size, i) => <option value={size} key={i}>{size}</option>)}
       </Quantity>
