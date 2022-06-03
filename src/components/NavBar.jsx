438cd8ee43531ce1ba75e75366a70d2d894256e8
gilcohen67 <gil.cohen67@gmail.com>
2022-06-03 12:42:27 -0700

    Logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 export default function NavBar() {
   return (
     <div className="page-top-bar">
-      <div className="page-title">Gambit Outlet</div>
+      {/* <div className="page-title">
+        <span className="page-title-g">Gambit</span>
+        <span className="page-title-o">Outlet</span>
+      </div> */}
+      <div className="page-title-short">
+        <span className="page-title-short-g">G</span>
+        <span className="page-title-short-o">o</span>
+        <span className="page-title-short-spade">♠</span>
+      </div>
       <div className="nav-bar">
         <a className="underline-button nav-bar-item" href="#product-detail">Product Overview</a>
         <a className="underline-button nav-bar-item" href="#related-items">Related Products & Outfit</a>
