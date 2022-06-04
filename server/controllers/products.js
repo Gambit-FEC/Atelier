19705a2b349b908c5743a31bbf0254bac5ca272b
Esther Kuang <eghkuang@gmail.com>
2022-06-04 11:41:03 -0700

    refactored to remove async await

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,15 +1,16 @@
 const axios = require('axios');
-const { API_URL } = require('../../config');
+const { API_URL, API_KEY } = require('../../config');
 
 exports.getOneProduct = async (req, res) => {
-  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${req.params.productId}`, { headers: { Authorization: req.headers.Authorization } });
-  try {
-    const overview = await axios.get(`${API_URL}products/${req.params.productId}`, { headers: { Authorization: req.headers.Authorization } });
-    const styles = await axios.get(`${API_URL}products/${req.params.productId}/styles`, { headers: { Authorization: req.headers.Authorization } });
-    const combined = [];
-    await combined.push(overview.data, styles.data);
-    res.status(200).send(combined);
-  } catch (err) {
-    res.status(400).send(err);
-  }
+  axios.get(`${API_URL}products/${req.params.productId}`, { headers: { Authorization: API_KEY } })
+    .then((products) => {
+      axios.get(`${API_URL}products/${req.params.productId}/styles`, { headers: { Authorization: API_KEY } })
+        .then((styles) => {
+          const combined = [];
+          combined.push(products.data, styles.data);
+          res.status(200).send(combined);
+        })
+        .catch((err) => { res.status(404).send(err); });
+    })
+    .catch((err) => { res.status(404).send(err); });
 };
